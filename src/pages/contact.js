d3a83ddeb892ee259b4342f294f9caad69acfa2a
Kumaravel <kumaran2489@gmail.com>
2025-02-12 10:22:38 +0530

    query form submitted

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,84 @@
 import Breadcrumb from "@/components/common/Breadcrumb";
 import Layout from "@/components/layout/Layout";
 import Pricing from "@/components/pricingplan/Pricing4";
-
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const intialFormValue = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  phone: "",
+  message: "",  
+  company: "",
+};
+const API_URL = "http://localhost:8055/items/"
 function Contactpage() {
+  const [formValue, setFormValue] = useState(intialFormValue);
+  const handleChange = (e) => {
+   // alert("hii")
+    e.preventDefault();
+    let { id, value } = e.target;
+    let formData = { ...formValue, ...{ [id]: value } };
+    setError({});
+    setFormValue(formData);
+  };
+
+  const [error, setError] = useState({});
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    setError(validate(formValue));
+
+    //demo_users
+    let encryptObj = {
+      first_name: formValue.first_name,
+      last_name: formValue.last_name,
+      company_mail: formValue.email,
+      phone_number: formValue.phone,
+      help_description: formValue.message,
+      company_name: formValue.company,
+    };
+//alert(API_URL+'demo_users')
+    let respData = await axios({
+      method: "post",
+      url: `${API_URL}demo_users`,
+      data: encryptObj,
+    });
+    if(respData.status==200){
+      setFormValue(intialFormValue)
+      alert("Submitted Successfully")
+    }
+
+   
+  };
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.first_name) {
+      errors.first_name = "First name is required";
+    }
+    if (!values.last_name) {
+      errors.last_name = "Last name is required";
+    }
+    if (!values.email) {
+      errors.email = "Email is required";
+    }
+    if (!values.phone) {
+      errors.phone = "Phone is required";
+    }
+    if (!values.message) {
+      errors.message = "Message is required";
+    }
+    if (!values.company) {
+      errors.company = "Company is required";
+    }
+    return errors;
+  };
+
   return (
     <Layout>
       <Breadcrumb
@@ -95,42 +169,86 @@ function Contactpage() {
                       <div className="col-md-6 mb-20">
                         <div className="form-inner">
                           <label>first name</label>
-                          <input type="text" placeholder="First Name"/>
+                          <input type="text" placeholder="First Name"
+                          id="first_name"
+                          value={formValue.first_name}
+                          onChange={handleChange}
+                          />
+                          {error.first_name && (
+                            <span className="text-danger">{error.first_name}</span>
+                          )}
                         </div>
                       </div>
                       <div className="col-md-6 mb-20">
                         <div className="form-inner">
                           <label>Last name</label>
-                          <input type="text" placeholder="Last Name"/>
+                          <input type="text" placeholder="Last Name"
+                          id="last_name"
+                          value={formValue.last_name}
+                          onChange={handleChange}
+                          />
+                          {error.last_name && (
+                            <span className="text-danger">{error.last_name}</span>
+                          )}
                         </div>
                       </div>
                       <div className="col-lg-12 mb-20">
                         <div className="form-inner">
                           <label>Company/Organization</label>
-                          <input type="text" placeholder="Company/Organization"/>
+                          <input type="text" placeholder="Company/Organization"
+                          id="company"
+                          value={formValue.company}
+                          onChange={handleChange}
+                          />
+                          {error.company && (
+                            <span className="text-danger">{error.company}</span>
+                          )}
                         </div>
                       </div>
                       <div className="col-lg-12 mb-20">
                         <div className="form-inner">
                           <label>Email</label>
-                          <input type="email" placeholder="Email"/>
+                          <input type="email" placeholder="Email"
+                          id="email"
+                          value={formValue.email}
+                          onChange={handleChange}
+                          />
+                          {error.email && (
+                            <span className="text-danger">{error.email}</span>
+                          )}
                         </div>
                       </div>
                       <div className="col-lg-12 mb-20">
                         <div className="form-inner">
                           <label>Phone</label>
-                          <input type="email" placeholder="Phone"/>
+                          <input type="email" placeholder="Phone"
+                          id="phone"
+                          value={formValue.phone}
+                          onChange={handleChange}
+                          />
+                          {error.phone && (
+                            <span className="text-danger">{error.phone}</span>
+                          )}
                         </div>
                       </div>
                       <div className="col-lg-12 mb-20">
                         <div className="form-inner">
                           <label>Message</label>
-                          <textarea defaultValue={""} placeholder="Additional Message" />
+                          <textarea defaultValue={""} placeholder="Additional Message" 
+                          id="message"
+                          value={formValue.message}
+                          onChange={handleChange}
+                          />
+                          {error.message && (
+                            <span className="text-danger">{error.message}</span>
+                          )}
                         </div>
                       </div>
                       <div className="col-lg-12">
                         <div className="form-inner">
-                          <button className="primary-btn3" type="submit">
+                          <button className="primary-btn3" type="submit"
+                           onClick={(e) => handleSubmit(e, "user")}
+                          >
                             Submit
                           </button>
                         </div>
